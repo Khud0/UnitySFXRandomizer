4d5a2d5d9b2bb6e963da3dc4a54f22c675bf17d6
Khud0 <pavement.cafe.37@gmail.com>
2020-04-03 18:00:52 +0200

    Improved the usage of "params" keyword

--- Assets/Scripts/ButtonSound.cs
+++ Assets/Scripts/ButtonSound.cs
@@ -13,17 +13,20 @@ public class ButtonSound : MonoBehaviour
 
 
     #region Play SFX Using Array Argument
+
     // This example doesn't use different sound effects and instead just changes the pitch of a single one,
     // but I wanted to add this feature so that you can easily implement this code into a real game.
     [SerializeField] private AudioClip[] soundEffects = default;
 
     private void PlayClickSFX()
     {
-        SFXPlayer.instance.PlaySFX(soundEffects);
+        SFXPlayer.instance.PlaySFX(true, soundEffects);
     }
+
     #endregion
 
     #region Play SFX Using Comma-Separated Arguments
+
     // This code is just as viable as the code above.
     /*
     [SerializeField] private AudioClip soundEffect1 = default;
@@ -31,8 +34,9 @@ public class ButtonSound : MonoBehaviour
 
     private void PlayClickSFX()
     {
-        SFXPlayer.instance.PlaySFX(soundEffect1, soundEffect2);
+        SFXPlayer.instance.PlaySFX(true, soundEffect1, soundEffect2);
     }
     */
+
     #endregion
 }
--- Assets/Scripts/SFXPlayer.cs
+++ Assets/Scripts/SFXPlayer.cs
@@ -39,55 +39,33 @@ public class SFXPlayer : MonoBehaviour
     // You also can send no arguments. If you send no arguments, the length of the params list is zero.
 
     // In other words, both ways to use PlaySFX method with "params" keyword are viable:
-    //          AudioClip[] sfxOptions;                 MusicPlayer.instance.PlaySFX(sfxOptions);
-    // OR       AudioClip clip1; AudioClip clip2;       MusicPlayer.instance.PlaySFX(clip1, clip2);
+    //          AudioClip[] sfxOptions;                 MusicPlayer.instance.PlaySFX(false, sfxOptions);
+    // OR       AudioClip clip1; AudioClip clip2;       MusicPlayer.instance.PlaySFX(false, clip1, clip2);
 
     /// <summary>
     /// Chooses a random AudioClip from the given array (or from comma-separated arguments) and plays it with a random pitch.
     /// </summary>
-    public void PlaySFX(params AudioClip[] clips)
+    /// <param name="changePitch">If set to "false", then original pitch will be used</param>
+    public void PlaySFX(bool changePitch, params AudioClip[] clips)
     {
         if (clips.Length == 0) { Debug.Log("Mehod \"PlaySFX\" requires at least one argument!"); return; }
 
         // Pick a random clip and randomize its pitch.
         int randomIndex = Random.Range(0, clips.Length);
-        float randomPitch = Random.Range(minPitch, maxPitch);
+        float newPitch = 1f;
+        if (changePitch) newPitch = Random.Range(minPitch, maxPitch);
         AudioClip clipToPlay = clips[randomIndex];
 
         // Create a new AudioSource with its own pitch and play the sound effect with its help.
         AudioSource sfxSource = gameObject.AddComponent<AudioSource>();
         sfxSource.clip = clipToPlay;
-        sfxSource.pitch = randomPitch;
+        sfxSource.pitch = newPitch;
         sfxSource.volume = sfxVolume;
         sfxSource.loop = false;
         sfxSource.Play();
 
         // Since we will no longer use it, destroy the AudioSource after the clip has finished playing. The delay is added "just in case".
         Destroy(sfxSource, clipToPlay.length + desctructionDelay);
-    }
-
-    // ("params" from Microsoft documentation: https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/params):
-    // No additional parameters are permitted after the params keyword in a method declaration, and only one params keyword is permitted in a method declaration.
-
-    // You can't make PlaySFX(params AudioClip[] clips, bool RANDOMIZE_PITCH), that's why this is a separate method.
-
-    /// <summary>
-    /// Chooses a random AudioClip from the given array (or from comma-separated arguments) and plays it.
-    /// </summary>
-    public void PlaySFXNoPitch(params AudioClip[] clips) 
-    {
-        if (clips.Length == 0) { Debug.Log("Mehod \"PlaySFXNoPitch\" requires at least one argument!"); return; }
-
-        // Pick a random clip to play.
-        int randomIndex = Random.Range(0, clips.Length);
-        AudioClip clipToPlay = clips[randomIndex];
-        
-        // Create a new AudioSource and play the sound effect with its help.
-        AudioSource sfxSource = gameObject.AddComponent<AudioSource>();
-        sfxSource.clip = clipToPlay;
-        sfxSource.volume = sfxVolume;
-        sfxSource.loop = false;
-        sfxSource.Play();
     }
 }
     
